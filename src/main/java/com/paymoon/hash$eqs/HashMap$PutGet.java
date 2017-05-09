040c6d60a315650982bbbc3f2a614b3fbeb475af
stevenlii <stevenlii@qq.com>
2017-05-09 15:41:29 +0800

    hashMap

---
+++ src/main/java/com/paymoon/hash$eqs/HashMap$PutGet.java
@@ -0,0 +1,75 @@
+package com.paymoon.hash$eqs;
+
+import java.util.HashMap;
+
+public class HashMap$PutGet<K,V> extends HashMap<K, V>{
+	/**
+	 * haspmap jdk7.0的put方法
+	 * 主要逻辑
+	 * 1、先查询，无则插入，有则覆盖
+	 * 代码逻辑
+	 * 1、对要放入的key进行hash,以用来查询（重要：是对key hash不是V）,
+	 * 并通过key的hash，用indexFor方法获得当前hash在数组中的索引
+	 * 2、e.hash == hash,e是Entry对象，e.hash应该就是e.key.hash,对比hash和eqs（key），判断是否
+	 * 存在当前key
+	 * 3、如果存在，则覆盖旧值并返回旧值，如果不存在，则addEntry(hash, key, value, i)
+	 * 4、addEntry里面的方法：table[bucketIndex] = new Entry<>(hash, key, value, e);可以看出来是加了
+	 * Entry对象
+	 * 
+	 */
+	
+//	 public V put(K key, V value) {
+//	        if (table == EMPTY_TABLE) {
+//	            inflateTable(threshold);
+//	        }
+//	        if (key == null)
+//	            return putForNullKey(value);
+//	        int hash = hash(key);
+//	        int i = indexFor(hash, table.length);
+//	        for (Entry<K,V> e = table[i]; e != null; e = e.next) {
+//	            Object k;
+//	            if (e.hash == hash && ((k = e.key) == key || key.equals(k))) {
+//	                V oldValue = e.value;
+//	                e.value = value;
+//	                e.recordAccess(this);
+//	                return oldValue;
+//	            }
+//	        }
+//
+//	        modCount++;
+//	        addEntry(hash, key, value, i);
+//	        return null;
+//	    }
+	
+	
+	/**
+	 * Get方法
+	 * 可以看到是拿到对象Entry
+	 * 通过对key hash,然后找到数组索引，通过对比hash和key eqs方法，找到则返回此entry, 
+	 * 然后对象entry对象里面有get value
+	 */
+//	  public V get(Object key) {
+//	        if (key == null)
+//	            return getForNullKey();
+//	        Entry<K,V> entry = getEntry(key);
+//
+//	        return null == entry ? null : entry.getValue();
+//	    }
+	
+//	final Entry<K,V> getEntry(Object key) {
+//        if (size == 0) {
+//            return null;
+//        }
+//
+//        int hash = (key == null) ? 0 : hash(key);
+//        for (Entry<K,V> e = table[indexFor(hash, table.length)];
+//             e != null;
+//             e = e.next) {
+//            Object k;
+//            if (e.hash == hash &&
+//                ((k = e.key) == key || (key != null && key.equals(k))))
+//                return e;
+//        }
+//        return null;
+//    }
+}
---
+++ src/main/java/com/paymoon/hash$eqs/SourceCode$bitwise.java
@@ -0,0 +1,24 @@
+package com.paymoon.hash$eqs;
+public class SourceCode$bitwise{
+	//涉及的知识点，位与运算，
+	//Java 位运算(移位、位与、或、异或、非） - Ely's Blog - 博客频道 - CSDN.NET
+    //	http://blog.csdn.net/xiaochunyong/article/details/7748713
+	
+//	位运算、异或的实际应用 - 无忌 - ITeye技术网站
+//	http://nassir.iteye.com/blog/1994914
+	public static void main(String[] args) {
+		System.err.println(9<<4);
+		System.err.println(85&8989);//
+		
+	}
+	/**
+     * java7中hashmap中寻找数组index使用这种方式
+     * Returns index for hash code h.
+     * h:key的hash（非常重要）
+     * length: table的长度
+     */
+	 static int indexFor(int h, int length) {
+	        // assert Integer.bitCount(length) == 1 : "length must be a non-zero power of 2";
+	        return h & (length-1);
+	    }
+}
